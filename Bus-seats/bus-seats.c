0005cc67237eeaba9b066640c3b2b89550144a7a
Kishore Varma <IN.KISHOREVARMA@GMAIL.COM>
2016-12-31 16:09:40 +0530

    Release - app for bus seats
    Compiled and released the application by making some ui changes.

--- Bus-seats/bus-seats.c
+++ Bus-seats/bus-seats.c
@@ -5,15 +5,18 @@ void cancel();
 void vseats();
 void logo(){
   printf("\n \n \t\t\t\t");
+  printf("---------------------------------------------------------\n");
+  printf("\n \t\t\t\t");
   system("echo `7MMmmmmMq.       db `7MMF'   `7MF' db      `7MN.   `7MF' ");printf("\t\t\t\t");
   system("echo   MM   `MM.      ;MM:  `MA     ,V  ;MM:       MMN.    M   ");printf("\t\t\t\t");
-  system("echo   MM   ,M9      ,V^MM.  VM:   ,V  ,V^MM.      M YMb   M   ");printf("\t\t\t\t");
+  system("echo   MM   ,M9      ,V^MM.    VM:  ,V  ,V^MM.       M YMb   M   ");printf("\t\t\t\t");
   system("echo   MMmmdM9      ,M  `MM   MM.  M' ,M  `MM      M  `MN. M   ");printf("\t\t\t\t");
   system("echo   MM  YM.      AbmmmqMA  `MM A'  AbmmmqMA     M   `MM.M   ");printf("\t\t\t\t");
   system("echo   MM   `Mb.   A'     VML  :MM;  A'     VML    M     YMM   ");printf("\t\t\t\t");
   system("echo .JMML. .JMM..AMA.   .AMMA. VF .AMA.   .AMMA..JML.    YM   ");
-  printf("\n");
-  printf("-----------------------------------------------------------------------------------------------------------------------------\n");
+  printf("\n\t\t\t\t");
+  printf("---------------------------------------------------------\n");
+
 }
 int i,bus[10][6]={0};
 void main()
@@ -31,8 +34,14 @@ void main()
     		printf("\n \t\t\t\t\t\t\t 2) Cancel");
     		printf("\n \t\t\t\t\t\t\t 3) View Seats \n \t\t\t\t\t\t\t 4) Exit \n\n");
     		printf(" \n\n\nUser > ");
-//scan user input
+        //scan user input
     		scanf("%d",&op);
+        while(op<1||op>4)
+        {
+                printf("\n - - choose either 1 or 2 or 3 - -");
+                printf(" \n User > ");
+                scanf("%d",&op);
+        }
     		switch(op)
     		{
     			case 1 :system("cls");
@@ -48,19 +57,21 @@ void main()
                                         if(*(*(bus+i))!=1)
                                         {
                                             *(*(bus+i))=1;
-                                            printf("successfully booked and seat no is w%d\n",6*i+1);
+                                            printf("\n \t\t\t\t");
+                                            printf("Operation Result : successfully booked and seat no is w%d\n",6*i+1);
                                             count=1;
                                             break;
                                         }
                                         if(*(*(bus+i)+5)!=1)
                                         {
                                             *(*(bus+i)+5)=1;
-                                            printf("successfully booked and seat no is w%d\n",6*i+6);
+                                            printf("\n \t\t\t\t");
+                                            printf("Operation Result : successfully booked and seat no is w%d\n",6*i+6);
                                             count=1;
                                             break;
                                         }
                                    }
-                                     if(count!=1) printf("Sorry no seat available");
+                                     if(count!=1) printf("\n \t\t\t\tOperation Result : Sorry no seat available");
                                      break;
                           case 2 :system("cls");
                                   for(i=0;i<10;i++)
@@ -69,18 +80,18 @@ void main()
                                         {
                                             *(*(bus+i)+2)=1;
                                             count=1;
-                                            printf("successfully booked and seat no is w%d\n",6*i+3);
+                                            printf("\n \t\t\t\tOperation Result : successfully booked and seat no is w%d\n",6*i+3);
                                             break;
                                         }
                                         if(*(*(bus+i)+3)!=1)
                                         {
                                             *(*(bus+i)+3)=1;
                                             count=1;
-                                            printf("successfully booked and seat no is w%d\n",6*i+4);
+                                            printf("\n \t\t\t\tOperation Result : successfully booked and seat no is w%d\n",6*i+4);
                                             break;
                                         }
                                   }
-                                    if(count!=1) printf("Sorry no seat available");
+                                    if(count!=1) printf("\n \t\t\t\tOperation Result : Sorry no seat available");
                                     break;
                           case 3 :system("cls");
                                   for(i=0;i<10;i++)
@@ -96,8 +107,8 @@ void main()
                                         }
                                         if(count==1) break;
                                   }
-                                  if(count==1) printf("seat reserved and seat no is w%d \n",6*i+j+1);
-                                  else printf("sorry all seats reserved . good bye :)\n ");
+                                  if(count==1) printf("\n \t\t\t\tOperation Result : seat reserved and seat no is w%d \n",6*i+j+1);
+                                  else printf("\n \t\t\t\tOperation Result : sorry all seats reserved . good bye :)\n ");
                                   break;
                           case 4 :system("cls");
                                   break;
@@ -109,24 +120,28 @@ void main()
                      printf(" user/cancel-seat >  ");
                      scanf("%d",&j);
                      j--;
+                     system("cls");
                      if(*(*bus+j)==1)
                      {
                             *(*bus+j)=0;
-                            printf("seat is cancelled\n");
+                            printf("\n \t\t\t\tOperation Result : seat is cancelled\n");
                      }
                      break;
             case 3 : system("cls");
                      logo();
-                     for(j=0;j<6;j++) printf(" w%d-%d ",++k,bus[i][j]);
-                     printf("\nHere w[int - represents seat no]-[int - 0 represents that seat is not reserved ,1 represents reserved] \n\n");
-                     for(i=0,k=0;i<10;i++)
-                     {
-                            printf("\n");
-                     }
+                    printf("\n\t\tHere w[int - represents seat no]-[int - 0 represents that seat is not reserved ,1 represents reserved] \n\n");
+                    for(i=0,k=0;i<10;i++)
+                    {
+                        printf("\n \t\t\t\t");
+                        for(j=0;j<6;j++) printf(" w%02d-%d ",++k,bus[i][j]);
+                        printf("\n");
+                    }
+                    printf("\n");
+                    system("pause");
+                    system("cls");
                      break;
             case 4 : exit(0);
                      break;
-    			 default : printf("\nPlease choose either 1 or 2 or 3 \n");
     	  }
 
     }while(op!=4);
